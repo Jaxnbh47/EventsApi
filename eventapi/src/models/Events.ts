6a5a6ad59393de53e83c6060164eda256e7fa6d8
“daliashaman” <daliashaman@gmail.com>
2023-02-13 21:41:24 -0500

    Updated

--- eventapi/src/models/Events.ts
+++ eventapi/src/models/Events.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import { Events } from '../services/EventsServices';
+import { Event } from '../services/EventsServices';
 
-export function GetEvents() : Promise<Events> {
-    return axios.get<Events>('https://app.ticketmaster.com/discovery/v2/events.json?apikey=MlsyU0dIeOhOWoaOEVqX3xT9QKxuvX40')
+export function GetEvents() : Promise<Event> {
+    return axios.get<Event>('https://app.ticketmaster.com/discovery/v2/events.json?apikey=MlsyU0dIeOhOWoaOEVqX3xT9QKxuvX40')
         .then(response => response.data);
 }
 
