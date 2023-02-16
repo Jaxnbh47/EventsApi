3b8c9bd287480048055a8183b04c45ea013962a9
Cristiana <cristiana.gherasim@gmail.com>
2023-02-16 18:48:24 -0500

    nav bar

--- eventapi/src/App.tsx
+++ eventapi/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import { Homepage } from './components/Homepage';
 import { Header } from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400&display=swap');
 </style>
